306cc76c4ef2efe928c4b86b67e778bd9dfb488c
kolan <kolanikk1@gmail.com>
2022-04-02 13:28:22 +0300

    maked views posts

---
+++ frontend/page-post.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../frontend/header.php';
+
+?>
+    <!-- Page Header-->
+    <header class="masthead" style="background-image: url('/frontend/assets/img/about-bg.jpg')">
+        <div class="container position-relative px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <div class="page-heading">
+                        <h1><?= $_GET['title'];?></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Main Content-->
+    <main class="mb-4">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <p><?= $_GET['text_post'];?></p>
+            </div>
+        </div>
+    </main>
+    <!-- Footer-->
+<?php
+require_once '../frontend/footer.php';
+?>
